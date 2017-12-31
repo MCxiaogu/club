f94c909a6c5b7354f6023b60934ae4022d0cb042
leon332157 <30728609+leon332157@users.noreply.github.com>
2017-12-31 18:55:29 -0500

    Add files via upload

---
+++ com.py
@@ -0,0 +1,90 @@
+import socket
+import cPickle as pickle
+import tkMessageBox as messagebox
+from Tkinter import *
+from functools import partial
+
+connected = False
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = 6666
+
+
+def pass_auth():
+    password = e2.get()
+    try:
+        s.send(pickle.dumps(password))
+    except Exception as e:
+        messagebox.showwarning(title='Error', message=e)
+        return
+    auth = pickle.loads(s.recv(1024))
+    if auth != 'correct':
+        messagebox.showwarning(title='incorrect password', message='Incorrect password!')
+        return
+    else:
+        messagebox.showinfo(title='Successful', message='Successfully login')
+        return
+
+
+def connect():
+    ip = e1.get()
+    if not ip == '':
+        pass
+    else:
+        messagebox.showwarning(title='Invalid input', message='Please input the right ip.')
+        return
+    try:
+        s.connect((str(ip), port))
+        messagebox.showinfo(title='connected', message='Successfully connected!')
+    except Exception as e:
+        messagebox.showwarning(title='Error', message=e)
+
+
+def exceute():
+    commmand = e3.get()
+    if not commmand == '':
+        pass
+    else:
+        messagebox.showinfo(title='input command', message='Please input command.')
+    try:
+        s.send(pickle.dumps('Y29tbWFuZA== ' + str(commmand)))
+    except Exception as e:
+        messagebox.showwarning(title='Error', message=e)
+        return
+
+
+def show_password():
+    if not e2.get() == '':
+        messagebox.showinfo(title='password', message=e2.get())
+    else:
+        messagebox.showinfo(title='input password', message='Please input password.')
+
+
+root1 = Tk()
+v = IntVar()
+root1.title('Server Login')
+l1 = Label(root1, text='Server Login', font=('', 30))
+l2 = Label(root1, text='Input server ip here:')
+l3 = Label(root1, text='Input server password here:')
+l4=Label(root1, text='Command execute on server')
+l4 = Label(root1, )
+e1 = Entry(root1)
+e2 = Entry(root1, show='*')
+e3 = Entry(root1)
+b1 = Button(text='OK', command=connect)
+b2 = Button(text='OK', command=partial(pass_auth))
+b3 = Button(text='exit', command=exit)
+b4 = Button(text='Execute on server', command=exceute)
+b5 = Button(text='Show password', command=show_password)
+l1.pack()
+l2.pack()
+e1.pack()
+b1.pack()
+l3.pack()
+e2.pack()
+b2.pack()
+b5.pack()
+b3.pack()
+root1.resizable(width=False, height=False)
+root1.mainloop()
+s.close()
+print('Connection Closed')
---
+++ server.py
@@ -0,0 +1,39 @@
+import cPickle as pickle
+import socket
+import subprocess
+import os
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 6666))
+s.listen(100)
+try:
+ conn, addr = s.accept()
+except KeyboardInterrupt:
+    exit()
+print('connection from: ' + str(addr))
+
+
+def main():
+        try:
+            raw_data = conn.recv(1024)
+            data = pickle.loads(raw_data)
+            if str(data).startswith('Y29tbWFuZA==', 0, len(data)):
+                raw_command = str(data).split(' ')
+                print raw_command
+            else:
+                pass
+            if data == 'a_ha':
+                conn.send(pickle.dumps('correct'))
+                print('correct')
+                return
+            else:
+                conn.send(pickle.dumps('incorrect'))
+                print('wrong password')
+                return
+        except KeyboardInterrupt:
+            s.close()
+            print('Connection Closed')
+            exit()
+if __name__=='__main__':
+ while True:
+  main()
