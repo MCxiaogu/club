5d6509dc15d8ffe0fcb6a20bca30bb723d34d950
leon332157 <leon332157@sina.com>
2018-01-31 19:09:14 -0500

    .

--- text.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.system('python3 ' + os.getcwd() + '/com.py;exit')
-os.system('python3 ' + os.getcwd() + '/server.py;exit')
-os.system('python3 ' + os.getcwd() + '/change password.py;exit')
